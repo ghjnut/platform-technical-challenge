e051d996e9ba9c895674f765dc052a26369a6846
Jake Pelletier <ghjnut@gmail.com>
2023-05-24 21:44:39 -0600

    create ecr repo for app lambda container

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,17 @@
 data "aws_caller_identity" "current" {}
+
+# TODO a little bit of funkiness promoting artifacts if there's a repo per env
+resource "aws_ecr_repository" "app" {
+  name = "inception-health/app"
+
+  # TODO enable encryption
+  #encryption_configuration = {
+  #  encryption_type = "KMS"
+  #  kms_key =
+  #}
+
+  # TODO should be enabled
+  #image_scanning_configuration {
+  #  scan_on_push = true
+  #}
+}
