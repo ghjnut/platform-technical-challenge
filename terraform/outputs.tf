eabde9b21b35d620591ca477c36bba90f8a5d697
Jake Pelletier <ghjnut@gmail.com>
2023-05-28 20:13:15 -0600

    return ecr repo image id and backend api gateway url as outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "app_backend_api_gateway_endpoint" {
+  value = aws_apigatewayv2_api.backend.api_endpoint
+}
