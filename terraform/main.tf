e2572f2655df8442dec639ddc505db1762c29b35
Jake Pelletier <ghjnut@gmail.com>
2023-05-28 20:11:52 -0600

    :art: move tf resources in order of necessity

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,22 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+# TODO a little bit of funkiness promoting artifacts if there's a repo per env
+resource "aws_ecr_repository" "app" {
+  name = "inception-health/app"
+
+  # TODO enable encryption
+  #encryption_configuration = {
+  #  encryption_type = "KMS"
+  #  kms_key =
+  #}
+
+  # TODO should be enabled
+  #image_scanning_configuration {
+  #  scan_on_push = true
+  #}
+}
+
 resource "aws_dynamodb_table" "checkin" {
   name           = "${var.env}-checkin"
   billing_mode   = "PROVISIONED"
@@ -18,22 +34,6 @@ resource "aws_dynamodb_table" "checkin" {
   #tags = {
   #  Name        =
   #  Environment =
-  #}
-}
-
-# TODO a little bit of funkiness promoting artifacts if there's a repo per env
-resource "aws_ecr_repository" "app" {
-  name = "inception-health/app"
-
-  # TODO enable encryption
-  #encryption_configuration = {
-  #  encryption_type = "KMS"
-  #  kms_key =
-  #}
-
-  # TODO should be enabled
-  #image_scanning_configuration {
-  #  scan_on_push = true
   #}
 }
 
